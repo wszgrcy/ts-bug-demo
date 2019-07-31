83539756d61814727215890c2f9c4444730e4e02
wszgrcy <wszgrcy@gmail.com>
2019-07-31 21:04:32 +0800

    init

--- demo.ts
+++ demo.ts
@@ -1,17 +1,11 @@
 
 export class Config<T> {
     constructor(
-        public config?: ConfigBase<T>
+        public classType?: Type<T>
     ) {
     }
 }
-export class ConfigBase<T> {
-    constructor(
-        public classType: Type<T>
-    ) {
 
-    }
-}
 export class SubClass {
     test: string
 }
@@ -19,25 +13,23 @@ export class SubConfig extends Config<SubClass>{
 
 }
 interface Type<T> {
-    /**
-      * Creates a new function.
-      * @param args A list of arguments the function accepts.
-      */
-    new(...args: string[]): T;
+    new(...args: any[]): T;
 }
 function f1<D, T extends Config<D>>(val: T) {
     return val
 }
 function f2<D, T extends Config<D>>(val: T) {
-    return val.config.classType
+    return val.classType
 }
 /**
  * when `f1` return val,and classType=>SubClass
  * but `f2` return  val.config.classType and classType=>unknown
  * I want to use `f2` and also return `SubClass`
+ * How to Change Code?
+ * Or like a bug?
  */
 let a = f1(new SubConfig())
-let classA = new a.config.classType()
+let classA = new a.classType()
 console.log(classA.test)
 let b = f2(new SubConfig())
 let classB=new b()
